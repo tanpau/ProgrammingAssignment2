4169f6b185f89d14d40b167038d57a73d5840c3f
Stephan Marsoner <masloc@gmail.com>
2014-05-25 23:50:37 +0200

    analog vector example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() creates a special matrix object
+## cacheSolve() calculates the inverse of the matrix.
 
-## Write a short comment describing this function
+##  makeCacheMatrix() creates a special "matrix", which is really a list containing a function to
+##  1. set the matrix
+##  2. get the matrix
+##  3. set the inverse of the matrix
+##  4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
 
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() calculates or retrieves inverse matrix of special "matrix" created with makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  } else {
+    inv <- solve(x$get())
+    x$setinverse(inv)
+    inv
+  }
 }
